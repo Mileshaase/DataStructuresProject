94f6d4a6fb77b05c320b28b13708fc9c61f29242
kinnaraa <kinnarabosworth@gmail.com>
2024-04-19 14:26:28 -0400

    maxHeap filter by rating done!

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Data : MonoBehaviour
 {
@@ -57,14 +58,14 @@ public class Data : MonoBehaviour
 
     public void SortData(List<string> Genres, int Year, int SearchType)
     {
-        if(SearchType == 0)
+        /*if(SearchType == 0)
         {
             QuickSort();
         }
         else
-        {
+        {*/
             MaxHeap(Genres, Year);
-        }
+        //}
     }
 
     private void QuickSort()
@@ -73,9 +74,84 @@ public class Data : MonoBehaviour
     }
     private void MaxHeap(List<string> Genres, int Year)
     {
-        foreach (var movie in data)
-        {
+        Debug.Log("Making Max Heap...");
 
+        // make new list of movies filtered by criteria
+
+        List<KeyValuePair<string, List<string>>> filteredMovies = new List<KeyValuePair<string, List<string>>>();
+        
+        // iterate through movie dataset to filter
+
+        foreach(var movie in data)
+        {
+            int movieYear = 0;
+
+            if (int.TryParse(movie.Value[0], out movieYear)) // make sure there are no errors for movies with "/N" as their year
+            {
+                if(Genres.Contains(movie.Value[2]) && movieYear <= Year && movieYear >= Year - 10)
+                    {
+                        // add movie to list if it fits criteria
+
+                        filteredMovies.Add(movie);
+                    }
+            }            
+        }
+
+        // convert list into an array
+
+        heap = filteredMovies.ToArray();
+
+        // make the heap
+
+        MakeHeap();
+
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(heap[i].Key + "           \n");
+        }
+    }
+
+    private void MakeHeap()
+    {
+        // heapify the nodes to make the max heap
+
+        for (int i = heap.Length / 2 - 1; i >= 0; i--)
+        {
+            heapify(i);
+        }
+    }
+
+    private void heapify(int i)
+    {
+        // initializing the left and right children of each node
+
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        // if the right node is in bounds and larger than the current largest node, right node becomes the new largest
+
+        if (right < heap.Length && float.Parse(heap[right].Value[3]) > float.Parse(heap[largest].Value[3]))
+        {
+            largest = right;
+        }
+
+        // or if the left node is in bounds and larger than the current largest node, left is new largest
+
+        if (left < heap.Length && float.Parse(heap[left].Value[3]) > float.Parse(heap[largest].Value[3]))
+        {
+            largest = left;
+        }
+
+        if (largest != i)
+        {
+            // if the root isn't largest anymore, swap them and heapify
+
+            KeyValuePair<string, List<string>> temp = heap[i];
+            heap[i] = heap[largest];
+            heap[largest] = temp;
+
+            heapify(largest);
         }
     }
 }
